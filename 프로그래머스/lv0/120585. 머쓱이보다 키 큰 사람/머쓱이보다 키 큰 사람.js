01861ffb3620389d0ffb55381b7e95236d37e222
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-07 23:42:24 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,9 @@
+function solution(array, height) {
+    let answer = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] > height){
+            answer += 1;
+        }
+    }
+    return answer;
+}
