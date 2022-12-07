4a9d9b5a04e51324b202bee29a2f85c80efe4a8b
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-07 23:28:25 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+var answer = [];
+    let cnt = 0;
+    while(cnt < numbers.length) {
+        answer.push(numbers[cnt] * 2);
+        cnt = cnt + 1;
+    }
+    
+    return answer;
+}
