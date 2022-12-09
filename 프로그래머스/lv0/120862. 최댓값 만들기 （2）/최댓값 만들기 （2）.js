da6508d732d7d218efe38da43321a32bdf78e457
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-09 17:53:48 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 3.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+   
+    let newArray = [];
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = i + 1; j < numbers.length; j++){
+            newArray.push(numbers[i]*numbers[j]);
+        }
+    }
+    
+    newArray.sort((a,b) => a-b);
+    console.log(newArray);
+    return newArray[newArray.length-1];
+}
