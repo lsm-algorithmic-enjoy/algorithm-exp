a4970f6b7bb7c04f986dcb7619b5114fcbb4d69a
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-11 01:22:20 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,26 @@
+function solution(common) {
+    let answer = 0;
+    let ratioArray = [];
+    let minusArray = [];
+    
+    if(common.some(item => item === 0)){
+        return common[common.length-1]+common[1]-common[0];
+    }
+    
+    for(let i = 0; i < common.length-1; i++){
+        ratioArray.push(common[i+1]/common[i]);
+        minusArray.push(common[i+1]-common[i]);
+    }
+    
+    const newratioArray = [...new Set(ratioArray)];
+    const newminusArray = [...new Set(minusArray)];
+    
+    if(newratioArray.length === 1){
+        return common[common.length-1]*newratioArray[0];
+    }
+    
+    if(newminusArray.length === 1){
+        return common[common.length-1]+newminusArray[0];
+    }
+    
+}
