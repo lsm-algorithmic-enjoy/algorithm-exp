993eeab46464bfc9ae5a249f5bbd1e177d008357
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-10 15:34:01 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,20 @@
+function solution(my_string) {
+    let newArray = [];
+    let newStr = '';
+    
+    
+    for(let i = 0; i < my_string.length; i++){
+        if(!isNaN(my_string[i])){
+            newStr = newStr + my_string[i];
+        }
+        if((isNaN(my_string[i])) && (!isNaN(my_string[i-1]))){
+            newArray.push(newStr);
+            newStr = '';
+        }
+        if((i === my_string.length-1) && !isNaN(my_string[i])){
+            newArray.push(newStr);
+        }
+    }
+    const answerArr = newArray.map(strnum => Number(strnum));
+    return answerArr.reduce((acc, cur) => acc + cur, 0);
+}
