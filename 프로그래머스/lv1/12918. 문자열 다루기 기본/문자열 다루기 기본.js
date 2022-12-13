e5aae84c4270ced315be882930b10e709eaf550e
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-13 11:25:07 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    const strto_arr = s.split('');
+    const bool1 = strto_arr.every((item) => !isNaN(parseInt(item)));
+    const bool2 = (strto_arr.length === 6) || (strto_arr.length === 4) ;
+    
+    if(bool1 && bool2){
+        return true;
+    }
+    else return false;
+}
