151effcb7e05b9d40a273bba331e60fc38d94a84
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-14 17:46:06 +0900

    [level 0] Title: 등수 매기기, Time: 3.30 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,35 @@
+function solution(score) {
+    let answer = [];
+    let avgArray = [];
+    let scoreObj = {};
+    let sortedscoreObj = {};
+    let sbs = [1];
+    for(let i = 0; i < score.length; i++){
+       let avg = (score[i][0] + score[i][1])/2
+       avgArray.push(avg);
+       scoreObj[i] = avg;
+    }
+    
+    console.log(scoreObj)
+    
+    const sortavgarr = avgArray.sort((a,b) => b-a);
+    
+    for(let i = 1; i < sortavgarr.length; i++){
+        if(sortavgarr[i-1] > sortavgarr[i]){
+            sbs.push(i+1);
+        }
+        if(sortavgarr[i-1] === sortavgarr[i]){
+            sbs.push(sbs[i-1]);
+        }
+    }
+    
+    for(let i = 0; i < sbs.length; i++){
+        sortedscoreObj[sortavgarr[i]] = sbs[i]
+    }
+    console.log(sortedscoreObj)
+    
+    for(let k = 0; k < score.length; k++){
+    answer.push(sortedscoreObj[scoreObj[k]]);
+    }
+    return answer;
+}
