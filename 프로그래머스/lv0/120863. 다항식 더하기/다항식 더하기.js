177f4344f141692f31b51479b733ffeffd6e0301
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-16 13:47:31 +0900

    [level 0] Title: 다항식 더하기, Time: 2.90 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,40 @@
+function solution(polynomial) {
+    let xstring = '';
+    let xstringnum = 0;
+    let finalnum = 0;
+    const polyArray = polynomial.split(' ');
+    for(let i = 0; i < polyArray.length; i = i + 2){
+        if(polyArray[i][polyArray[i].length-1] === 'x'){
+         if(polyArray[i] === 'x'){
+             xstring = '1';
+         }   
+         else   {
+             for(let j = 0; j < polyArray[i].length-1; j++){
+               xstring= xstring + polyArray[i][j];
+}
+         }
+            xstringnum = xstringnum + parseInt(xstring);
+            xstring = '';
+}
+        else finalnum = finalnum + parseInt(polyArray[i]); 
+    }
+    console.log(xstringnum)
+    console.log(finalnum)
+    //애초에 문자열 0인 경우
+    //상수만 나오는 경우
+    //일차항만 나오는 경우, 단 x일때 매우주의
+    //일차항+상수 경우, 단 x일때 매우주의
+   
+    if((xstringnum === 0) && (finalnum === 0)){
+          return "0";   
+    }
+    if(xstringnum === 0){
+        return `${finalnum}`;
+    }
+    if(finalnum === 0){
+       if(xstringnum === 1) return 'x';
+       return `${xstringnum}x`;
+    }
+    if (xstringnum === 1) return `x + ${finalnum}`;
+    return `${xstringnum}x + ${finalnum}`;
+}
