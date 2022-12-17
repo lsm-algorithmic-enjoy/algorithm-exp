1ec9c668ca76947a9bd9e2170d3179a8385beeef
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2022-12-17 19:00:43 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 3.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,8 +1,8 @@
 function solution(lines) {
-  const visited = lines.reduce((a, [x, y]) => {
-    for (let i = Math.min(x, y) + 1; i <= Math.max(x, y); i++) a[i] = a[i] ? a[i] + 1 : 1;
-    return a;
-  }, {});
-
-  return Object.values(visited).filter(v => v > 1).length;
+    const visited = lines.reduce((acc, [x,y]) => {
+        for(let i = x + 1; i <= y; i++) acc[i] = acc[i] ? acc[i] + 1 : 1;
+        return acc;
+    }, {});
+    console.log(visited);
+    return Object.values(visited).filter(v => v > 1).length;
 }
