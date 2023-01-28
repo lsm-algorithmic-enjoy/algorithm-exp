3dc5881ebd050522a0d9a0d24db8582896e05cbb
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-01-28 13:13:32 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,7 +1,7 @@
 function solution(cipher, code) {
     let answer = '';
-    for(let i = code;i <= cipher.length ; i = i + code){
-        answer= answer + cipher[i-1];
+    for(let i = code ; i <= cipher.length ; i = i + code){
+        answer = answer + cipher[i-1];
     }
     return answer;
 }
