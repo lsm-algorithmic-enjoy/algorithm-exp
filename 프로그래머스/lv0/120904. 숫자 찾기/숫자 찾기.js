f6ca86504cb0dade322e10381e2e492eee5d5a42
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-03-20 21:48:09 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,6 +1,5 @@
 function solution(num, k) {
-    let answer = 0;
-    let str_num = String(num);
+    const str_num = String(num);
     if (str_num.includes(k)){
         return str_num.indexOf(k) + 1;
     }
