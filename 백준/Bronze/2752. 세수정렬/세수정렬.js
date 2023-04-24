542c5bd86e204272221c185a753e9bf228d6a6d1
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-04-24 23:20:54 +0900

    [Bronze IV] Title: 세수정렬, Time: 148 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+let arr = input[0].split(' ').map(Number);
+
+arr.sort(function(a,b) {
+    return a - b;
+})
+let answer = "";
+for(let i = 0; i < arr.length; i++){
+    answer += arr[i] + " ";
+}
+
+console.log(answer);
