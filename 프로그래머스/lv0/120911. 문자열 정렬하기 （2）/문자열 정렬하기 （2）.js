c58347ff1a1be0caf9e0edea9da42ddc77b0b218
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-04-27 23:52:23 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,6 +1,4 @@
 function solution(my_string) {
-    let answer = '';
     const low = my_string.toLowerCase();
-    answer = low.split('').sort().join('');
-    return answer;
+    return low.split('').sort().join('');
 }
