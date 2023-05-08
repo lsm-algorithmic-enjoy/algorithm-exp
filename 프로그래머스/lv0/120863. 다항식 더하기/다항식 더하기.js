47d117f01d64a7b2466b596133c43711e897025f
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-05-08 22:45:43 +0900

    [level 0] Title: 다항식 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -20,17 +20,15 @@ function solution(polynomial) {
     }
 
     //애초에 문자열 0인 경우(but, 문제 조건 불분명하므로 일단 고려)
-    //일차항만 나오는 경우, 단 x일때 매우주의
     //상수만 나오는 경우
+    //일차항만 나오는 경우, 단 x일때 매우주의
     //일차항+상수 경우, 단 x일때 매우주의
    
-    if(finalnum === 0 && xstringnum === 0) return "0";
+    if (xstringnum === 0) return `${finalnum}`;
     if(finalnum === 0){
        if(xstringnum === 1) return 'x';
        else return `${xstringnum}x`;
     }
-    
-    if (xstringnum === 0) return `${finalnum}`;
     if (xstringnum === 1) return `x + ${finalnum}`;
     return `${xstringnum}x + ${finalnum}`;
 }
