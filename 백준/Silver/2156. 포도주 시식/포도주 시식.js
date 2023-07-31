a260450646f6c96ae044345b9c9d46d07d92e5d4
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-07-31 23:22:00 +0900

    [Silver I] Title: 포도주 시식, Time: 180 ms, Memory: 10496 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.js
@@ -0,0 +1,21 @@
+let file = require('fs').readFileSync('/dev/stdin');
+let input = file.toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++){
+    arr.push(Number(input[i]));
+}
+
+let dp = new Array(n).fill(0);
+dp[0] = arr[0];
+dp[1] = arr[0] + arr[1];
+dp[2] = Math.max(arr[0] + arr[1], arr[0] + arr[2], arr[1] + arr[2]);
+
+for(let i = 3; i < n; i++){
+    dp[i] = dp[i-1];
+    dp[i] = Math.max(dp[i], arr[i] + dp[i-2]);
+    dp[i] = Math.max(dp[i], arr[i] + arr[i-1] + dp[i-3]);
+}
+
+console.log(dp[n-1]);
