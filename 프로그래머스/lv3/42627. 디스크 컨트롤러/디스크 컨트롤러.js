0b90529cbf5e2fd636149e292f4d21d624dabf23
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-09-21 23:29:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -0,0 +1,36 @@
+function solution(jobs) {
+    
+    let totalTime = 0;
+    let currentTime = 0;
+    let waitingRoom = [];
+    
+    const scheduling = () => {
+        if (!waitingRoom.length) return false;
+        
+        const [inputTime, workTime] = waitingRoom.shift();
+        totalTime += currentTime - inputTime + workTime;
+        currentTime += workTime;
+        return true;
+    };
+    
+    const insertJob = (job) => {
+        waitingRoom.push(job);
+        let i = waitingRoom.length - 1;
+        waitingRoom.sort((a,b) => a[1] - b[1]);
+    };
+    
+    jobs.sort((a,b) => a[0] - b[0])
+    .forEach(([inputTime,workTime]) => {
+        while(currentTime < inputTime) {
+            if (!scheduling()) {
+                currentTime = inputTime;
+            }
+        }
+        insertJob([inputTime, workTime]);
+    });
+    
+    while(waitingRoom.length > 0) {
+        scheduling();
+    }
+    return parseInt(totalTime / jobs.length);
+}
