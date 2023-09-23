71f3fb625abaf3088727aa2fd07e7ed7908d09da
Seung <85894771+lsm-algorithmic-enjoy@users.noreply.github.com>
2023-09-23 13:49:01 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,14 +1,14 @@
 function solution(n, lost, reserve) {
-  let lost_student = lost.filter(v => !reserve.includes(v)).sort();
-  let reserve_student = reserve.filter(v => !lost.includes(v)).sort();
-
-  for (let i = 0; i < reserve_student.length; i++) {
-    if (lost_student.includes(reserve_student[i] - 1)) {
-      lost_student = lost_student.filter(v => v !== reserve_student[i] - 1);
-    } else if (lost_student.includes(reserve_student[i] + 1)) {
-      lost_student = lost_student.filter(v => v !== reserve_student[i] + 1);
+    let newlost = lost.filter(item => !reserve.includes(item)).sort();
+    const newreserve = reserve.filter(item => !lost.includes(item)).sort();
+    
+    for(let i = 0; i < newreserve.length; i++){
+        if(newlost.includes(newreserve[i]-1)){
+            newlost = newlost.filter(item => item !== newreserve[i] - 1);
+        }
+        else if(newlost.includes(newreserve[i]+1)){
+            newlost = newlost.filter(item => item !== newreserve[i] + 1);
+        }
     }
-  }
-
-  return n - lost_student.length;
+    return n - newlost.length;
 }
